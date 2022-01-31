b37fe61b87a83b995568cd8edb4c6bd19d34d311
morozov-work <morozov-work@outlook.com>
2022-01-31 18:16:47 +0300

    add brakpoints

--- src/util/constants.js
+++ src/util/constants.js
@@ -1,7 +1,16 @@
 const navigation = Object.freeze({
   expandedWidth: "271px",
   collapsedWidth: "56px",
-  defaultState: true, // Expanded
+  defaultState: false, // Expanded
 });
 
-export { navigation };
+const breakpoints = Object.freeze({
+  xs: "600", //phone
+  sm: "960", //tablet
+  md: "1264", //laptop*
+  lg: "1904", //desktop*
+});
+
+//* -16px on desktop for browser scrollbar
+
+export { navigation, breakpoints };
