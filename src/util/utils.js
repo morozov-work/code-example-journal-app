70eac4eea251ceff25e89b2777bf041fac17e44b
morozov-work <morozov-work@outlook.com>
2022-01-31 18:17:21 +0300

    add utils

---
+++ src/util/utils.js
@@ -0,0 +1,30 @@
+import {
+  navigation as navigationConstants,
+  breakpoints,
+} from "@/util/constants";
+
+const {
+  defaultState,
+  expandedWidth: expandedShift,
+  collapsedWidth: collapsedShift,
+} = navigationConstants;
+
+const { xs: phone, sm: tablet, md: laptop, lg: desktop } = breakpoints;
+
+function calcViewportShift() {
+  if (document.documentElement.clientWidth <= 600) return 0;
+  return defaultState ? expandedShift : collapsedShift;
+}
+
+function findDeviceType() {
+  const width = document.documentElement.clientWidth;
+
+  if (width > desktop) return "ultra-wide";
+  if (width > laptop && width <= desktop) return "desktop";
+  if (width > tablet && width <= laptop) return "laptop";
+  if (width > phone && width <= tablet) return "tablet";
+
+  return "phone";
+}
+
+export { calcViewportShift, findDeviceType };
