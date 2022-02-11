72c807ddb9f8dd2565f55f78a1d9d7aa0c748752
unknown <morozov-work@outlook.com>
2022-02-11 22:19:45 +0300

    add nested routes

--- src/router/index.js
+++ src/router/index.js
@@ -1,11 +1,21 @@
 import Vue from "vue";
 import VueRouter from "vue-router";
+
 import { auth } from "@/util/auth";
+
 import Login from "@/views/Login.vue";
 import Register from "@/views/Register.vue";
 import Resetting from "@/views/Resetting.vue";
-import Home from "@/views/Home/Home.vue";
 import PageNotFound from "@/views/PageNotFound/PageNotFound.vue";
+
+import MainLayout from "@/layouts/MainLayout/MainLayout.vue";
+import Home from "@/views/Home/Home.vue";
+import Journals from "@/views/Journals/Journals.vue";
+import Organizations from "@/views/Organizations/Organizations.vue";
+import Users from "@/views/Users/Users.vue";
+import Notifications from "@/views/Notifications/Notifications.vue";
+import Debugging from "@/views/Debugging/Debugging.vue";
+
 import BactericidalLog from "@/views/BactericidalLog/BactericidalLog.vue";
 
 Vue.use(VueRouter);
@@ -27,14 +37,8 @@ const routes = [
     component: Resetting,
   },
   {
-    path: "/",
-    name: "Home",
-    component: Home,
-    meta: { requiresAuth: true },
-  },
-  {
-    name: "404",
     path: "/404",
+    name: "404",
     component: PageNotFound,
   },
   {
@@ -42,10 +46,56 @@ const routes = [
     redirect: "404",
   },
   {
-    name: "Journal",
-    path: "/log-bactericidallog",
-    component: BactericidalLog,
-    meta: { requiresAuth: true },
+    path: "/",
+    component: MainLayout,
+    children: [
+      {
+        path: "/",
+        name: "Home",
+        component: Home,
+        meta: { requiresAuth: true },
+      },
+      {
+        path: "/home",
+        redirect: "/",
+      },
+      {
+        path: "/journals",
+        name: "Journals",
+        component: Journals,
+        meta: { requiresAuth: true },
+      },
+      {
+        path: "/organizations",
+        name: "Organizations",
+        component: Organizations,
+        meta: { requiresAuth: true },
+      },
+      {
+        path: "/users",
+        name: "Users",
+        component: Users,
+        meta: { requiresAuth: true },
+      },
+      {
+        path: "/notifications",
+        name: "Notifications",
+        component: Notifications,
+        meta: { requiresAuth: true },
+      },
+      {
+        path: "/debugging",
+        name: "Debugging",
+        component: Debugging,
+        meta: { requiresAuth: true },
+      },
+      {
+        name: "Journal",
+        path: "/log-bactericidallog",
+        component: BactericidalLog,
+        meta: { requiresAuth: true },
+      },
+    ],
   },
 ];
 
