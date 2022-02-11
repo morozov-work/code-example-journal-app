e3c87ad6b4f978fefe98eb1436ed42e7d8cb6554
unknown <morozov-work@outlook.com>
2022-02-11 13:48:39 +0300

    home refactoring #1

--- src/views/Home/Home.vue
+++ src/views/Home/Home.vue
@@ -1,84 +1,25 @@
 <template>
   <main-layout>
-    <template v-slot:content>
-      <div class="d-flex flex-column align-start">
-        <div>
-          <warning-message />
-          <heading />
-          <div class="mt-6" style="display: flex; flex-wrap: wrap">
-            <journal-card
-              title="Журнал бактерицидных ламп"
-              @openJournal="openJournal"
-            >
-              <template v-slot:icon>
-                <germicidal />
-              </template>
-            </journal-card>
-            <v-card
-              v-for="(journal, index) in journals"
-              :key="index"
-              width="400"
-              height="276"
-            >
-              {{ journal }}
-            </v-card>
-            <new-journal></new-journal>
-          </div>
-        </div>
-      </div>
-    </template>
+    <template v-slot:content>HOME</template>
   </main-layout>
 </template>
 
 <script>
 import MainLayout from "@/layouts/MainLayout/MainLayout.vue";
-import WarningMessage from "@/components/WarningMessage/WarningMessage.vue";
-import Heading from "@/components/Heading/Heading.vue";
-import JournalCard from "@/components/JournalCard/JournalCard.vue";
-import NewJournal from "@/components/NewJournal/NewJournal.vue";
-
-// import TemperatureHumidity from "@/assets/icons/journals/temperature-humidity.svg";
-import Germicidal from "@/assets/icons/journals/germicidal.svg";
-
-import "./Home.scss";
 
 export default {
   components: {
     MainLayout,
-    WarningMessage,
-    Heading,
-    JournalCard,
-    NewJournal,
-    // TemperatureHumidity,
-    Germicidal,
   },
 
   name: "Home",
 
   data() {
-    return {
-      journals: [
-        "Бактерицидная установка",
-        "Температура сотрудников",
-        "Дезинсекция и дератизация",
-        "Дезинфицирующие средства и дезинфекционные работы",
-        "Скоропортящаяся пищевая продукция",
-        "Температурный режим холодильного оборудования",
-        "Температура и влажность склада",
-        "Дефростация продуктов",
-        "Фритюрные жиры",
-        "Бракеражный журнал",
-        "Гигиенический журнал здоровья",
-      ],
-    };
+    return {};
   },
 
   computed: {},
 
-  methods: {
-    openJournal() {
-      this.$router.push({ name: "Journal" });
-    },
-  },
+  methods: {},
 };
 </script>
